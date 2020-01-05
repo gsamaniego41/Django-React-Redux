fc0760996d977f8c07767c34cf3fa0c065f7191c
Gabriel Samaniego <selfdev.gs@gmail.com>
2020-01-05 12:12:54 -0800

    Create migrations

--- leadmanager/leads/models.py
+++ leadmanager/leads/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Lead(models.Model):
+  name = models.CharField(max_length=100) 
+  email = models.EmailField(max_length=100, unique=True)
+  # blank=True makes it optional
+  message = models.CharField(max_length=500, blank=True)
+  # auto_now_add=True will add the date automatically
+  created_at = models.DateTimeField(auto_now_add=True) 
