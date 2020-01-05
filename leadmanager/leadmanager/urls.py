3e50cdb94a2d5f9c658e75026d441bdbf8ea8503
Gabriel Samaniego <selfdev.gs@gmail.com>
2020-01-05 14:02:49 -0800

    React setup

--- leadmanager/leadmanager/urls.py
+++ leadmanager/leadmanager/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 # include allows us to bring in a separate file
 
 urlpatterns = [
+    path('', include('frontend.urls')),
     path('', include('leads.urls')),
 ]
